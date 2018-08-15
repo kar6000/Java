46e9cc1ddb3bb46843d90b5f21b7d5c1b02079b4
kar6000 <34102279+kar6000@users.noreply.github.com>
2018-08-14 21:45:53 -0700

    Add files via upload

---
+++ SwapInt1.java
@@ -0,0 +1,17 @@
+package com.aug5;
+
+public class SwapInt1 {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b =20;
+		
+		a=a+b; // a=30;
+		b =a-b;//b=10;
+		a=a-b;//a=20
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
